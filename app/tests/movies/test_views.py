c401f7cadda36d9bc73d48a44ec288eb81d6ec66
elenvides <elenvides@gmail.com>
2023-09-19 13:10:16 +0300

    drf routes and tests

---
+++ app/tests/movies/test_views.py
@@ -0,0 +1,68 @@
+import pytest
+
+from movies.models import Movie
+
+
+@pytest.mark.django_db
+def test_add_movie(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big",
+            "genre": "comedy",
+            "year": "1998"
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 201
+    assert resp.data["title"] == "The Big"
+
+    movies = Movie.objects.all()
+    assert len(movies) == 1
+
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json_keys(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big Lebowski",
+            "genre": "comedy",
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+
+@pytest.mark.django_db
+def test_get_single_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+    resp = client.get(f"/api/movies/{movie.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+
+
+def test_get_single_movie_incorrect_id(client):
+    resp = client.get(f"/api/movies/foo/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_get_all_movies(client, add_movie):
+    movie_one = add_movie(title="The First Movie in the test", genre="horror", year="2001")
+    movie_two = add_movie("The Second Movie in the test", "fantasy", "1881")
+
+    resp = client.get("/api/movies/")
+
+    assert resp.status_code == 200
+    assert resp.data[0]["title"] == movie_one.title
+    assert resp.data[1]["title"] == movie_two.title
